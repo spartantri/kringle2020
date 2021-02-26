73a3adfd338416ea8b3f65b95ec88e1b0dff2e75
Manuel Spartan <mleosrivas@mastersprogram.sans.edu>
2021-02-26 05:29:58 +0000

    public

--- createpdf.sh
+++ createpdf.sh
@@ -3,3 +3,6 @@ cd /tmp
 git clone https://github.com/twardoch/mkdocs-combine.git
 cd mkdocs-combine
 pip install -e .
+sudo apt-get install fonts-lmodern lmodern pandoc texlive-base texlive-latex-extra texlive-fonts-recommended texlive-latex-recommended texlive-xetex
+pandoc --toc -f markdown+grid_tables+table_captions -o SpartanTriKringleConWriteup.pdf SpartanTriKringleConWriteup.pd
+mkdocscombine -o SpartanTriKringleConWriteup.pd
